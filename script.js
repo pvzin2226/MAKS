96a5588ed3e66171d2c04688a1d7598f2afaa0ca
HENRYEZ97 <ricksillva97@gmail.com>
2026-09-06 22:33:20 -0600

    agora com sql

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-function conhecerProdutos() {
-    document.getElementById("produtos").scrollIntoView({
-        behavior: "smooth"
+fetch("http://localhost:8080/produtos")
+    .then(resposta => resposta.json())
+    .then(produtos => {
+
+        alert("Produtos recebidos: " + produtos.length);
+
+        const lista = document.getElementById("lista-produtos");
+
+        produtos.forEach(produto => {
+
+            const card = document.createElement("div");
+
+            card.classList.add("produto");
+
+            card.innerHTML = `
+                <img src="${produto.imagem}" alt="${produto.nome}">
+                <h3>${produto.nome}</h3>
+                <p>R$ ${produto.preco}</p>
+                <h4>${produto.descricao}</h4>
+            `;
+
+            lista.appendChild(card);
+        });
+    })
+    .catch(erro => {
+        alert("ERRO: " + erro);
     });
-}
